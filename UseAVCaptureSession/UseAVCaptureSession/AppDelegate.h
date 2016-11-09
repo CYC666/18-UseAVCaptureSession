9d73f94d5c1f59f2bbbfa7563186e8d93751b8ee
CYC666 <13705038428@163.com>
2016-11-09 09:16:34 +0800

    修改了切换视频跟拍照的样式

---
+++ UseAVCaptureSession/UseAVCaptureSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseAVCaptureSession
+//
+//  Created by mac on 16/11/8.
+//  Copyright © 2016年 CYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
